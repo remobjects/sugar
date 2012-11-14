8f350639d55d617b15f8a4379e44e9657d904016
Jeroen Vandezande <Jeroen.Vandezande@Spathi.be>
2012-11-14 11:00:31 +0100

    implemnted first method aa a test

--- Sugar/Math.pas
+++ Sugar/Math.pas
@@ -14,16 +14,16 @@ type
   Math = public class
   public
     const PI: Double = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
-    method pow(x, y: Double): Double;  
+    method Pow(x, y: Double): Double;  
   {$ENDIF}
   end;
 
 implementation
 
 {$IFDEF NOUGAT}
-method Math.pow(x, y: Double): Double;
+method Math.Pow(x, y: Double): Double;
 begin
-  
+  exit rtl.Math.pow(x,y);  
 end;
 {$ENDIF}
 
