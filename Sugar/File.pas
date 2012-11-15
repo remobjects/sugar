afd1536bf65dba7af5058797c67823e916603867
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2012-11-15 20:49:11 +0000

    finished adding methods to File class for .NET

--- Sugar/File.pas
+++ Sugar/File.pas
@@ -2,6 +2,9 @@
 
 interface
 
+uses
+  RemObjects.Oxygene.Sugar;
+
 type
 
   {$IF ECHOES}
@@ -14,6 +17,8 @@ type
     class method Move(aOldFileName, aNewFileName: String); mapped to Move(aOldFileName, aNewFileName);
     class method ReadBytes(aFileName: String): array of Byte; mapped to ReadAllBytes(aFileName);
     class method ReadText(aFileName: String): String; mapped to ReadAllText(aFileName);
+    class method WriteBytes(aFileName: String; aData: array of Byte); mapped to WriteAllBytes(aFileName, aData);
+    class method WriteText(aFileName: String; aText: String); mapped to WriteAllText(aFileName, aText);
   {$ENDIF}
   {$IF COOPER or Nougat}
   File = public class
