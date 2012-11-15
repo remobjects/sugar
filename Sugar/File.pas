2fc41d35edd3d47617c2cbc14533ea4cd3afa8e5
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2012-11-15 20:39:20 +0000

    Added more method to the File Class

--- Sugar/File.pas
+++ Sugar/File.pas
@@ -10,6 +10,10 @@ type
     class method AppendText(aFileName, aContents: String);  mapped to AppendAllText(aFileName, aContents);
     class method &Copy(aOldFileName, aNewFileName: String; aOverwriteFile: Boolean); mapped to &Copy(aOldFileName, aNewFileName, aOverwriteFile);
     class method Delete(aFileName: String); mapped to Delete(aFileName);
+    class method Exists(aFileName: String); mapped to Exists(aFileName);
+    class method Move(aOldFileName, aNewFileName: String); mapped to Move(aOldFileName, aNewFileName);
+    class method ReadBytes(aFileName: String): array of Byte; mapped to ReadAllBytes(aFileName);
+    class method ReadText(aFileName: String): String; mapped to ReadAllText(aFileName);
   {$ENDIF}
   {$IF COOPER or Nougat}
   File = public class
