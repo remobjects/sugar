ae31ffc7420c4329664da89dd05cbcc8d9b59aae
Jeroen Vandezande <Jeroen.Vandezande@Spathi.be>
2012-11-15 14:16:12 +0100

    mapped missing methods

--- Sugar/Math.pas
+++ Sugar/Math.pas
@@ -7,12 +7,60 @@ type
   {$IF COOPER}
   Math = public class mapped to java.lang.Math
   public
+    method Abs(d: Double): Double; mapped to abs(d);
+    method Acos(d: Double): Double; mapped to acos(d);
+    method Asin(d: Double): Double; mapped to asin(d);
+    method Atan(d: Double): Double; mapped to atan(d);
+    method Atan2(x,y: Double): Double; mapped to atan2(x,y);
     method Ceiling(a: Double): Double; mapped to ceil(a);
+    method Cos(d: Double): Double; mapped to cos(d);
+    method Cosh(d: Double): Double; mapped to cosh(d);
+    method Exp(d: Double): Double; mapped to exp(d);
+    method Floor(d: Double): Double; mapped to floor(d);   
+    method IEEERemainder(x, y: Double): Double; mapped to IEEEremainder(x, y); 
+    method Log(d: Double): Double; mapped to log(d);
+    method Log10(d: Double): Double; mapped to log10(d);
+    method Max(a,b: Double): Double; mapped to max(a,b);   
+    method Min(a,b: Double): Double; mapped to min(a,b);
+    method Pow(x, y: Double): Double; mapped to pow(x,y);
+    method Round(a: Double): Double; mapped to round(a);
+    method RoundToInt(a: Double): Integer;
     method Sign(d: Double): Integer;
+    method Sin(d: Double): Double; mapped to sin(d);
+    method Sinh(d: Double): Double; mapped to sinh(d);
+    method Sqrt(d: Double): Double; mapped to sqrt(d);
+    method Tan(d: Double): Double; mapped to tan(d);
+    method Tanh(d: Double): Double; mapped to tanh(d);
     method Truncate(d: Double): Double;
   {$ENDIF}
   {$IF ECHOES}
   Math = public class mapped to System.Math
+  public
+    method Abs(d: Double): Double; mapped to Abs(d);
+    method Acos(d: Double): Double; mapped to Acos(d);
+    method Asin(d: Double): Double; mapped to Asin(d);
+    method Atan(d: Double): Double; mapped to Atan(d);
+    method Atan2(x,y: Double): Double; mapped to Atan2(x,y);
+    method Ceiling(d: Double): Double; mapped to Ceiling(d);
+    method Cos(d: Double): Double; mapped to Cos(d);
+    method Cosh(d: Double): Double; mapped to Cosh(d);
+    method Exp(d: Double): Double; mapped to Exp(d);
+    method Floor(d: Double): Double; mapped to Floor(d);
+    method IEEERemainder(x, y: Double): Double; mapped to IEEERemainder(x, y); 
+    method Log(d: Double): Double; mapped to Log(d);
+    method Log10(d: Double): Double; mapped to Log10(d);
+    method Max(a,b: Double): Double; mapped to Max(a,b);   
+    method Min(a,b: Double): Double; mapped to Min(a,b);
+    method Pow(x, y: Double): Double; mapped to Pow(x,y);
+    method Round(a: Double): Double; mapped to Round(a);
+    method RoundToInt(a: Double): Integer;
+    method Sign(d: Double): Integer; mapped to Sign(d);
+    method Sin(d: Double): Double; mapped to Sin(d);
+    method Sinh(d: Double): Double; mapped to Sinh(d);
+    method Sqrt(d: Double): Double; mapped to Sqrt(d);
+    method Tan(d: Double): Double; mapped to Tan(d);
+    method Tanh(d: Double): Double; mapped to Tanh(d);
+    method Truncate(d: Double): Double; mapped to Truncate(d);
   {$ENDIF}
   {$IF NOUGAT}
   Math = public class
@@ -24,6 +72,7 @@ type
     method Asin(d: Double): Double;
     method Atan(d: Double): Double;
     method Atan2(x,y: Double): Double;
+    method Ceiling(d: Double): Double;
     method Cos(d: Double): Double;
     method Cosh(d: Double): Double;
     method Exp(d: Double): Double;
@@ -51,7 +100,23 @@ implementation
 {$IF COOPER}
 method Math.Truncate(d: Double): Double;
 begin
-  exit iif(d < 0, Mapped.ceil(d), Mapped.floor(d));
+  exit iif(d < 0, mapped.ceil(d), mapped.floor(d));
+end;
+{$ENDIF}
+
+{$IF COOPER or ECHOES}
+method Math.RoundToInt(a: Double): Integer;
+begin
+  exit Integer(mapped.round(a));
+end;
+{$ENDIF}
+
+{$IF COOPER or NOUGAT}
+method Math.Sign(d: Double): Integer;
+begin
+  if d > 0 then exit 1;
+  if d < 0 then exit -1;
+  exit 0;
 end;
 {$ENDIF}
 
@@ -69,6 +134,11 @@ end;
 method Math.Cos(d: Double): Double;
 begin
   exit rtl.Math.cos(d);   
+end;
+
+method Math.Ceiling(d: Double): Double;
+begin
+  exit rtl.Math.ceil(d);   
 end;
 
 method Math.Cosh(d: Double): Double;
@@ -169,15 +239,6 @@ end;
 method Math.Truncate(d: Double): Double;
 begin
   exit rtl.Math.trunc(d);  
-end;
-{$ENDIF}
-
-{$IF COOPER or NOUGAT}
-method Math.Sign(d: Double): Integer;
-begin
-  if d > 0 then exit 1;
-  if d < 0 then exit -1;
-  exit 0;
 end;
 {$ENDIF}
 
