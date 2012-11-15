31b6a39bd6054fa35cfda7e623caa9d3398b289a
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2012-11-15 20:14:53 +0000

    started on File class

---
+++ Sugar/File.pas
@@ -0,0 +1,20 @@
+﻿namespace RemObjects.Oxygene.Sugar.IO;
+
+interface
+
+type
+
+  {$IF ECHOES}
+  File = public class mapped to System.IO.File
+  public
+    class method AppendText(aFileName, aContents: String);  mapped to AppendAllText(aFileName, aContents);
+    class method &Copy(aOldFileName, aNewFileName: String; aOverwriteFile: Boolean); mapped to &Copy(aOldFileName, aNewFileName, aOverwriteFile);
+  {$ENDIF}
+  {$IF COOPER or Nougat}
+  File = public class
+  {$ENDIF}
+  end;
+
+implementation
+
+end.
