d9ace096cf57780c3fb1275bf1bf9ff59e8a7e1e
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2012-11-18 18:30:35 +0000

    added the username property to Environment.pas

--- Sugar/Environment.pas
+++ Sugar/Environment.pas
@@ -8,18 +8,21 @@ type
   Environment = public class mapped to System
   public 
     class property NewLine: String read mapped.getProperty("line.separator");
+    class property UserName: String read mapped.getProperty("user.name");
     class method GetEnvironmentVariable(aVariableName: String): String; mapped to getenv(aVariableName);
   {$ENDIF}
   {$IF ECHOES}
   Environment = public class mapped to System.Environment
   public
     class property NewLine: String read mapped.NewLine;
+    class property UserName: String read mapped.UserName;
     class method GetEnvironmentVariable(aVariableName: String): String; mapped to GetEnvironmentVariable(aVariableName);
   {$ENDIF}
   {$IF NOUGAT}
   Environment = public class
   public 
     class property NewLine: String read RemObjects.Oxygene.Sugar.String(#10);
+    class property UserName: String read Foundation.NSUserName();
     class method GetEnvironmentVariable(aVariableName: String): String;
   {$ENDIF}
   end;
