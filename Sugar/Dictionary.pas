a9260f100b7adf28c030807c249808523de9cce4
ejay <ejay@remobjects.com>
2012-11-19 17:36:56 +0200

    Dictionary implementation for Cooper and Echoes

--- Sugar/Dictionary.pas
+++ Sugar/Dictionary.pas
@@ -1,58 +1,26 @@
 ﻿namespace RemObjects.Oxygene.Sugar;
 
+{$HIDE W0} //supress case-mismatch errors
+{$HIDE W27}
+
 interface
 
+{$IF NOT NOUGAT}
+
 type
-  {$IF COOPER}
-  Dictionary<T, U> = public class mapped to java.util.Dictionary<T, U>
-    //method __getKeys: sequence of T; iterator;
-    property Values[aKey: T]: U read mapped.get(aKey) write mapped.put;
-    property AllKeys: sequence of T read new EnumerationSequence<T>(mapped.keys);
-    method ContainsKey(aKey: T): Boolean;
-  end;
-  {$ENDIF}
-
-  {$IF ECHOES}
-  Dictionary<T, U> = public class mapped to System.Collections.Generic.Dictionary<T, U>
-    property Values[aKey: T]: U read mapped[aKey] write mapped[aKey];
-    property AllKeys: sequence of T read mapped.Keys;
-    method ContainsKey(aKey: T): Boolean; mapped to ContainsKey(aKey);
-  end;
-  {$ENDIF}
-
-  {$IF NOUGAT}
-  Dictionary<T,U> = public class mapped to Foundation.NSMutableDictionary
-  //where T is class, U is class
-  private
-    method getValue(aKey: dynamic): dynamic; mapped to objectForKey(aKey);
-    method setValue(aKey: dynamic; aValue: dynamic); mapped to setObject(aValue)forKey(aKey);
+  Dictionary<T, U> = public class mapped to {$IF ECHOES}System.Collections.Generic.Dictionary<T,U>{$ENDIF}{$IF COOPER}java.util.HashMap<T,U>{$ENDIF}{$IF NOUGAT}Foundation.NSMutableDictionary{$ENDIF}
   public
-    //property Values[aKey: dynamic{T}]: dynamic{U} read getValue write setValue;
-    method ContainsKey(aKey: T): Boolean; 
+    method &Add(Key: T; Value: U); mapped to {$IF ECHOES}Add(Key, Value){$ENDIF}{$IF COOPER}Put(Key, Value){$ENDIF}{$IF NOUGAT}setObject(Key, Value){$ENDIF};
+    method Clear; mapped to {$IFNDEF NOUGAT}Clear{$ELSE}removeAllObjects{$ENDIF};
+    method ContainsKey(Key: T): Boolean; mapped to {$IFNDEF NOUGAT}ContainsKey(Key){$ENDIF};
+    method ContainsValue(Value: U): Boolean; mapped to {$IFNDEF NOUGAT}ContainsValue(Value){$ENDIF};
+    method &Remove(Key: T); mapped to {$IFNDEF NOUGAT}&Remove(Key){$ENDIF};
+
+    property Item[Key: T]: U {$IFNDEF NOUGAT}read mapped[Key] write mapped[Key]{$ELSE}{$ENDIF}; default;
+    property Keys: sequence of T read {$IF ECHOES}mapped.Keys{$ENDIF}{$IF COOPER}mapped.keySet{$ENDIF};
+    property Values: sequence of U read {$IFNDEF NOUGAT}mapped.Values{$ELSE}{$ENDIF};
+    property Count: Integer read {$IF ECHOES OR NOUGAT}mapped.Count{$ENDIF}{$IF COOPER}mapped.size{$ENDIF};
   end;
-  {$ENDIF}
-  
+{$ENDIF}  
 implementation
-
-{$IF COOPER}
-method Dictionary.ContainsKey(aKey: T): Boolean;
-begin
-  result := assigned(mapped.get(aKey));
-end;
-
-{method Dictionary.__getKeys: sequence of T;
-begin
-  var k := mapped.keys;
-  while k.hasMoreElements() do
-    yield k.nextElement();
-end;}
-{$ENDIF}
-
-{$IF NOUGAT}
-method Dictionary.ContainsKey(aKey: T): Boolean; 
-begin
-  //result := assigned(mapped.objectForKey(T));
-end;
-{$ENDIF}
-
 end.
