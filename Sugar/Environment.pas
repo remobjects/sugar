1aedbffa457ec9f61703e4a82c8877654a00ae8c
marc hoffman <mh@remobjects.com>
2012-11-19 15:24:26 +0100

    Nougat: OperatingSystemName, OperatingSystemVersion

--- Sugar/Environment.pas
+++ Sugar/Environment.pas
@@ -28,11 +28,14 @@ type
   {$ENDIF}
   {$IF NOUGAT}
   Environment = public class
+  private
+    class method getOperatingSystemVersion: String;
+    class method getOperatingSystemName: String;
   public 
     class property NewLine: String read RemObjects.Oxygene.Sugar.String(#10);
     class property UserName: String read Foundation.NSUserName();
-    class property OperatingSystemName: String read ""; //todo
-    class property OperatingSystemVersion: String read ""; //todo
+    class property OperatingSystemName: String read getOperatingSystemName;
+    class property OperatingSystemVersion: String read getOperatingSystemVersion;
     class property TargetPlatform: TargetPlatform read TargetPlatform.Apple;
     class property TargetPlatformAsString: String read 'Apple';
     class method GetEnvironmentVariable(aVariableName: String): String;
@@ -44,6 +47,29 @@ type
 implementation
 
 {$IF NOUGAT}
+class method Environment.getOperatingSystemName: String;
+begin
+  {$HIDE H0}
+  if rtl.TargetConditionals.TARGET_OS_MAC = 1 then result := ('OS X')
+  else if rtl.TargetConditionals.TARGET_OS_IPHONE = 1 then begin
+    if rtl.TargetConditionals.TARGET_IPHONE_SIMULATOR = 1 then result := 'iOS Simulator'
+    else result := 'iOS';
+  end;
+  {$SHOW H0}
+end;
+
+class method Environment.getOperatingSystemVersion: String;
+begin
+  var mib: array of Integer := [rtl.sys.CTL_KERN, rtl.sys.KERN_OSVERSION];
+  var namelen: UInt32 {u_int} := sizeOf(mib) / sizeOf(mib[0]);
+  var bufferSize: UInt64{size_t} := 0; // ToDo: why is size_t missing, and we MUSt use it, for 32/64 compatibility!
+
+  rtl.sys.sysctl(@mib, namelen, nil, @bufferSize, nil, 0);
+  var buildBuffer := new Char{u_char}[bufferSize];
+  if rtl.sys.sysctl(mib, namelen, buildBuffer, @bufferSize, nil, 0) > 0 then
+    result := Foundation.NSString.alloc.initWithBytes(buildBuffer) length(bufferSize) encoding(Foundation.NSStringEncoding.NSUTF8StringEncoding);
+ end;
+
 class method Environment.GetEnvironmentVariable(aVariableName: String): String;
 begin
   Foundation.NSProcessInfo.processInfo:environment:objectForKey(aVariableName);
