c4ef4a2b7a5e53899f53b5217d3121530d94fd85
ejay <ejay@remobjects.com>
2013-01-09 19:04:11 +0200

    Additional StringBuilder code for Cooper and Echoes

--- Sugar/StringBuilder.pas
+++ Sugar/StringBuilder.pas
@@ -1,5 +1,5 @@
 ﻿namespace RemObjects.Oxygene.Sugar;
-
+{$HIDE W0} //supress case-mismatch errors
 interface
 
 type
@@ -12,8 +12,16 @@ type
     method Append(value: Char; repeatCount: Integer): StringBuilder;
     method AppendLine(): StringBuilder;
     method AppendLine(value: String): StringBuilder;
-  {$ENDIF}
-  {$IF ECHOES}
+
+    method Clear; mapped to setLength(0);
+    method Delete(Start, &End: Integer): StringBuilder; mapped to delete(Start, &End);
+    method Replace(Start, &End: Integer; Value: String): StringBuilder; mapped to Replace(Start, &End, Value);
+    method Substring(Start: Integer): String; mapped to substring(Start);
+    method Substring(Start, &End: Integer): String; mapped to substring(Start, &End);
+    method Insert(Offset: Integer; Value: String): StringBuilder; mapped to Insert(Offset, Value);
+
+    property Length: Integer read mapped.length;
+  {$ELSEIF ECHOES}
   StringBuilder = public class mapped to System.Text.StringBuilder
   public
     method Append(value: String): StringBuilder; mapped to Append(value);
@@ -21,8 +29,16 @@ type
     method Append(value: Char; repeatCount: Integer): StringBuilder; mapped to Append(value, repeatCount);
     method AppendLine(): StringBuilder; mapped to AppendLine();
     method AppendLine(value: String): StringBuilder; mapped to AppendLine(value);
-  {$ENDIF}
-  {$IF NOUGAT}
+
+    method Clear;
+    method Delete(Start, &End: Integer): StringBuilder; mapped to &Remove(Start, &End);
+    method Replace(Start, &End: Integer; Value: String): StringBuilder;
+    method Substring(Start: Integer): String; mapped to ToString(Start, Length);
+    method Substring(Start, &End: Integer): String; mapped to ToString(Start, &End);
+    method Insert(Offset: Integer; Value: String): StringBuilder; mapped to Insert(Offset, Value);
+
+    property Length: Integer read mapped.Length;
+  {$ELSEIF NOUGAT}
   StringBuilder = public class mapped to Foundation.NSMutableString
   public
     method Append(value: String): StringBuilder;
@@ -55,9 +71,18 @@ begin
   mapped.append(Environment.NewLine);
   exit mapped;
 end;
-{$ENDIF}
+{$ELSEIF ECHOES}
+method StringBuilder.Clear;
+begin
+  mapped.Length := 0;
+end;
 
-{$IF NOUGAT}
+method StringBuilder.Replace(Start: Integer; &End: Integer; Value: String): StringBuilder;
+begin
+  mapped.Remove(Start, &End);
+  exit mapped.Insert(Start, Value);
+end;
+{$ELSEIF NOUGAT}
 method StringBuilder.Append(value: String): StringBuilder;
 begin
   mapped.appendString(value);
