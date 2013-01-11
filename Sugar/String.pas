30a609dd6b8db5df1fcdd6e76c9d3b5e24ad28f6
ejay <ejay@remobjects.com>
2013-01-11 17:48:58 +0200

    More code for String class

--- Sugar/String.pas
+++ Sugar/String.pas
@@ -7,57 +7,99 @@ interface
 type
   {$IF COOPER}
   String = public class mapped to java.lang.String
-  {$ELSEIF ECHOES}
-  String = public class mapped to System.String
-  {$ELSEIF NOUGAT}
-  String = public class mapped to Foundation.NSString
-  {$ENDIF}
   private
-    {$IF NOUGAT}
-    method NSMakeRange(loc: Int32; len: Int32): Foundation.NSRange; // temp workaround
-    {$ENDIF}
     method get_Chars(aIndex: Int32): Char;
-
   public
     property Length: Int32 read mapped.length;
     property Chars[aIndex: Int32]: Char read get_Chars; default;
 
     class method Format(aFormat: String; params aParams: array of Object): String;
     class operator Add(aStringA: String; aStringB: String): String;
-
     class method CharacterIsWhiteSpace(aChar: Char): Boolean;
+    class method IsNullOrEmpty(Value: String): Boolean;
+    class method IsNullOrWhiteSpace(Value: String): Boolean;
 
-    {$IF ECHOES OR COOPER}
+    method CompareTo(Value: String): Integer; mapped to compareTo(Value);
+    method CompareToIgnoreCase(Value: String): Integer; mapped to compareToIgnoreCase(Value);
+    method &Equals(Value: String): Boolean; mapped to &equals(Value);
+    method EqualsIngoreCase(Value: String): Boolean; mapped to equalsIgnoreCase(Value);
+    method Contains(Value: String): Boolean; mapped to Contains(Value);
+
     method IndexOf(aString: String): Int32; mapped to IndexOf(aString);
     method LastIndexOf(aString: String): Int32; mapped to LastIndexOf(aString);
-    {$ELSEIF NOUGAT}
-    method IndexOf(aString: String): Int32;
-    method LastIndexOf(aString: String): Int32;
-    {$ENDIF}
 
-    {$IF COOPER}
-    method Substring(aStartIndex: Int32): String;                 mapped to Substring(aStartIndex);
+    method Substring(aStartIndex: Int32): String; mapped to Substring(aStartIndex);
     method Substring(aStartIndex: Int32; aLength: Int32): String; mapped to Substring(aStartIndex, aStartIndex+aLength);
-    {$ELSEIF ECHOES}
-    method Substring(aStartIndex: Int32): String;                 mapped to Substring(aStartIndex);
-    method Substring(aStartIndex: Int32; aLength: Int32): String; mapped to Substring(aStartIndex, aLength);
-    {$ELSEIF NOUGAT}
-    method Substring(aStartIndex: Int32): String;                 mapped to substringFromIndex(aStartIndex);
-    method Substring(aStartIndex: Int32; aLength: Int32): String; 
-    {$ENDIF}
+    method Replace(OldValue, NewValue: String): String; mapped to replace(OldValue, NewValue);
 
-    {$IF COOPER}
     method ToLower: String; mapped to toLowerCase;
     method ToUpper: String; mapped to toUpperCase;
-    {$ELSEIF ECHOES}
+    method Trim: String; mapped to trim;    
+  end;
+  {$ELSEIF ECHOES}
+  String = public class mapped to System.String
+  private
+    method get_Chars(aIndex: Int32): Char;
+  public
+    property Length: Int32 read mapped.length;
+    property Chars[aIndex: Int32]: Char read get_Chars; default;
+
+    class method Format(aFormat: String; params aParams: array of Object): String;
+    class operator Add(aStringA: String; aStringB: String): String;
+    class method CharacterIsWhiteSpace(aChar: Char): Boolean;
+    class method IsNullOrEmpty(Value: String): Boolean; mapped to IsNullOrEmpty(Value);
+    class method IsNullOrWhiteSpace(Value: String): Boolean;
+
+    method CompareTo(Value: String): Integer; mapped to Compare(mapped, Value, false);
+    method CompareToIgnoreCase(Value: String): Integer; mapped to Compare(mapped, Value, true);
+    method &Equals(Value: String): Boolean; mapped to &Equals(Value);
+    method EqualsIngoreCase(Value: String): Boolean; mapped to &Equals(Value, StringComparison.InvariantCultureIgnoreCase);
+    method Contains(Value: String): Boolean; mapped to Contains(Value);
+
+    method IndexOf(aString: String): Int32; mapped to IndexOf(aString);
+    method LastIndexOf(aString: String): Int32; mapped to LastIndexOf(aString);    
+
+    method Substring(aStartIndex: Int32): String; mapped to Substring(aStartIndex);
+    method Substring(aStartIndex: Int32; aLength: Int32): String; mapped to Substring(aStartIndex, aLength);
+    method Replace(OldValue, NewValue: String): String; mapped to Replace(OldValue, NewValue);
+
     method ToLower: String; mapped to ToLower;
     method ToUpper: String; mapped to ToUpper;
-    {$ELSEIF NOUGAT}
+    method Trim: String; mapped to Trim;
+  end;
+  {$ELSEIF NOUGAT}
+  String = public class mapped to Foundation.NSString
+  private
+    method NSMakeRange(loc: Int32; len: Int32): Foundation.NSRange; // temp workaround
+    method get_Chars(aIndex: Int32): Char;
+  public
+    property Length: Int32 read mapped.length;
+    property Chars[aIndex: Int32]: Char read get_Chars; default;
+
+    class method Format(aFormat: String; params aParams: array of Object): String;
+    class operator Add(aStringA: String; aStringB: String): String;
+    class method CharacterIsWhiteSpace(aChar: Char): Boolean;
+    class method IsNullOrEmpty(Value: String): Boolean;
+    class method IsNullOrWhiteSpace(Value: String): Boolean;
+
+    method CompareTo(Value: String): Integer; mapped to Compare(Value);
+    method CompareToIgnoreCase(Value: String): Integer; mapped to caseInsensitiveCompare(Value);
+    method &Equals(Value: String): Boolean; 
+    method EqualsIngoreCase(Value: String): Boolean;
+    method Contains(Value: String): Boolean;
+
+    method IndexOf(aString: String): Int32;
+    method LastIndexOf(aString: String): Int32;
+
+    method Substring(aStartIndex: Int32): String; mapped to substringFromIndex(aStartIndex);
+    method Substring(aStartIndex: Int32; aLength: Int32): String; 
+    method Replace(OldValue, NewValue: String): String; mapped to stringByReplacingOccurrencesOfString(OldValue) withString(NewValue);
+
     method ToLower: String; mapped to lowercaseString;
     method ToUpper: String; mapped to uppercaseString;
-    {$ENDIF}
-
+    method Trim: String;    
   end;
+  {$ENDIF}
 
 implementation
 
@@ -79,11 +121,9 @@ class operator String.Add(aStringA: String; aStringB: String): String;
 begin
   {$IF COOPER}
   result := java.lang.String(aStringA)+java.lang.String(aStringB);
-  {$ENDIF}
-  {$IF ECHOES}
+  {$ELSEIF ECHOES}
   result := System.String(aStringA)+System.String(aStringB);
-  {$ENDIF}
-  {$IF NOUGAT}
+  {$ELSEIF NOUGAT}
   result := Foundation.NSString(aStringA).stringByAppendingString(aStringB);
   {$ENDIF}
 end;
@@ -92,15 +132,31 @@ class method String.CharacterIsWhiteSpace(aChar: Char): Boolean;
 begin
   {$IF COOPER}
   result := java.lang.Character.isWhitespace(aChar);
-  {$ENDIF}
-  {$IF ECHOES}
+  {$ELSEIF ECHOES}
   result := Char.IsWhiteSpace(aChar);
-  {$ENDIF}
-  {$IF NOUGAT}
+  {$ELSEIF NOUGAT}
   result := Foundation.NSCharacterSet.whitespaceAndNewlineCharacterSet.characterIsMember(aChar);
   {$ENDIF}
 end;
 
+{$IF COOPER OR NOUGAT}
+class method String.IsNullOrEmpty(Value: String): Boolean;
+begin
+  exit (Value = nil) or (Value.Length = 0);
+end;
+{$ENDIF}
+
+class method String.IsNullOrWhiteSpace(Value: String): Boolean;
+begin
+  if Value = nil then
+    exit;
+
+  for i: Integer := 0 to Value.Length-1 do
+    if not CharacterIsWhiteSpace(Value.Chars[i]) then
+      exit false;
+
+  exit true;
+end;
 
 {$IF NOUGAT}
 method String.NSMakeRange(loc: Int32; len: Int32): Foundation.NSRange;
@@ -123,17 +179,36 @@ method String.Substring(aStartIndex: Int32; aLength: Int32): String; //59155: No
 begin
   result := mapped.substringWithRange(NSMakeRange(aStartIndex, aLength));
 end;
+
+method String.Trim: String;
+begin
+  var Range := mapped.rangeOfString("^\\s*") options(NSStringCompareOptions.NSRegularExpressionSearch);
+  exit mapped.stringByReplacingCharactersInRange(Range) withString("");
+end;
+
+method String.Equals(Value: String): Boolean;
+begin
+  exit mapped.compare(Value) = 0;
+end;
+
+method String.EqualsIngoreCase(Value: String): Boolean;
+begin
+  exit mapped.caseInsensitiveCompare(Value) = 0;
+end;
+
+method String.Contains(Value: String): Boolean;
+begin
+  exit mapped.rangeOfString(Value).location <> NSNotFound;
+end;
 {$ENDIF}
 
 method String.get_Chars(aIndex: Int32): Char;
 begin
-  {$IF COOPER} // 59230: Improved IFDEF snytax
-  result := mapped[AIndex];
-  {$ENDIF}
-  {$IF ECHOES}
+  {$IF COOPER}
+  result := mapped.charAt(aIndex);
+  {$ELSEIF ECHOES}
   result := mapped[aIndex];
-  {$ENDIF}
-  {$IF NOUGAT}
+  {$ELSEIF NOUGAT}
   result := mapped.characterAtIndex(aIndex);
   {$ENDIF}
 end;
