4e7add55819b2924dfb6e23978cd8ca7a6c311c8
ejay <ejay@remobjects.com>
2013-11-15 16:41:15 +0200

    HTTP implementation for WinRT/Wp8

--- Sugar/HTTP.pas
+++ Sugar/HTTP.pas
@@ -3,6 +3,7 @@
 interface
 
 uses
+  RemObjects.Oxygene.Sugar.IO,
   RemObjects.Oxygene.Sugar.Xml;
 
 type
@@ -32,8 +33,15 @@ begin
     exit sb.toString;
   end;
   {$ELSEIF ECHOES}
+  {$IF WINDOWS_PHONE OR NETFX_CORE}
+  exit async method: String begin
+    var Client := new System.Net.Http.HttpClient();
+    exit Client.GetStringAsync(aURL).Result;
+  end;
+  {$ELSE}
   using lClient: System.Net.WebClient := new System.Net.WebClient() do 
     result := lClient.DownloadString(aURL);  
+  {$ENDIF}
   {$ELSEIF NOUGAT}
   {$ENDIF}
 end;
@@ -53,8 +61,15 @@ begin
     end;
   end;
   {$ELSEIF ECHOES}
+  {$IF WINDOWS_PHONE OR NETFX_CORE}
+  exit async method: Binary begin
+    var Client := new System.Net.Http.HttpClient();
+    exit  Binary.FromArray(Client.GetByteArrayAsync(aURL).Result);
+  end;
+  {$ELSE}
   using lClient: System.Net.WebClient := new System.Net.WebClient() do 
     result := Binary.FromArray(lClient.DownloadData(aURL));  
+  {$ENDIF}
   {$ELSEIF NOUGAT}
   var lRequest := NSURLRequest.requestWithURL(aURL);
   var lResponse: NSURLResponse;
@@ -74,6 +89,12 @@ begin
     exit XmlDocument.FromBinary(Bin);
   end;
   {$ELSEIF ECHOES}
+  {$IF WINDOWS_PHONE OR NETFX_CORE}
+  exit async method: XmlDocument begin
+    var Client := new System.Net.Http.HttpClient();
+    exit XmlDocument.FromString(Client.GetStringAsync(aURL).Result);
+  end;
+  {$ELSE}
   result := async method: XmlDocument begin
     using lClient: System.Net.WebClient := new System.Net.WebClient() do begin
       var lData := lClient.DownloadData(aURL);
@@ -81,6 +102,7 @@ begin
         result := XmlDocument.FromBinary(lBinary);
     end;
   end;
+  {$ENDIF}  
   {$ELSEIF NOUGAT}
   
   {$ENDIF}
