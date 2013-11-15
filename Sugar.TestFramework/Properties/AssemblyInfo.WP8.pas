8c51b65b98e8a6fb1fd87f2ed6964bca78956e55
ejay <ejay@remobjects.com>
2013-11-15 16:41:56 +0200

    Include missing properties files

---
+++ Sugar.TestFramework/Properties/AssemblyInfo.WP8.pas
@@ -0,0 +1,48 @@
+﻿namespace RemObjects.Oxygene.Sugar.TestFramework;
+
+interface
+
+uses System.Reflection,
+  System.Runtime.CompilerServices,
+  System.Runtime.InteropServices,
+  System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RemObjects.Oxygene.Sugar.Echoes.WP8.TestFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RemObjects.Oxygene.Sugar.Echoes.WP8.TestFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3CA6673E-74CC-4742-B625-6843FE496E15")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+{$IF CODESIGN}
+[assembly: AssemblyKeyName('RemObjectsSoftware')]
+{$ENDIF}
+
+implementation
+end.
