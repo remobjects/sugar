33cfe2d074c59122901a702b4c75c0a81d0e1890
ejay <ejay@remobjects.com>
2013-11-20 12:46:04 +0200

    Binary fixes and cleanup

--- Sugar/Binary.pas
+++ Sugar/Binary.pas
@@ -18,152 +18,69 @@ type
     property Length: Integer {$IF NOUGAT} read mapped.length write mapped.length{$ENDIF};
   end;
 
+  Binary = public class {$IF NOUGAT}mapped to Foundation.NSMutableData{$ENDIF}
   {$IF COOPER}
-  Binary = public class
   private
     fData: java.io.ByteArrayOutputStream := new java.io.ByteArrayOutputStream();
-  public
-    method Assign(aData: Binary);
-    method Clear;
-
-    method ReadRangeOfBytes(Range: Range): array of Byte;
-    method ReadBytes(aLength: Integer): array of Byte; 
-    
-    method Subdata(Range: Range): Binary;
-
-    method WriteBytes(aData: array of Byte; aLength: Integer);
-    method WriteData(aData: Binary);
-
-    method ToArray: array of Byte;
-    property Length: Integer read fData.size;
-
-    class method FromArray(aArray: array of Byte): Binary;
-  end;
   {$ELSEIF ECHOES}
-  Binary = public class
   private
     fData: System.IO.MemoryStream := new System.IO.MemoryStream();
   assembly
     property Data: System.IO.MemoryStream read fData;
+  {$ENDIF}
   public
     method Assign(aData: Binary);
     method Clear;
 
-    method ReadRangeOfBytes(Range: Range): array of Byte;
-    method ReadBytes(aLength: Integer): array of Byte; 
+    method &Read(Range: Range): array of Byte;
+    method &Read(aLength: Integer): array of Byte;
     
     method Subdata(Range: Range): Binary;
 
-    method WriteBytes(aData: array of Byte; aLength: Integer);
-    method WriteData(aData: Binary);
+    method &Write(aData: array of Byte; aLength: Integer);
+    method &Write(aData: Binary);
 
     method ToArray: array of Byte;
-    property Length: Integer read fData.Length;
+    property Length: Integer read {$IF COOPER}fData.size{$ELSEIF ECHOES}fData.Length{$ELSEIF NOUGAT}mapped.length{$ENDIF};
 
     class method FromArray(aArray: array of Byte): Binary;
+    class method &Empty: Binary;
   end;
-  {$ELSEIF NOUGAT}
-  Binary = public class mapped to Foundation.NSMutableData
-  public
-    method Assign(aData: Binary); mapped to setData(aData);
-    method Clear; mapped to setLength(0);
-
-    method ReadRangeOfBytes(Range: Range): array of Byte;
-    method ReadBytes(aLength: Integer): array of Byte; 
-    
-    method Subdata(Range: Range): Binary;
-
-    method WriteBytes(aData: array of Byte; aLength: Integer);
-    method WriteData(aData: Binary);
-
-    method ToArray: array of Byte;
-    property Length: Integer read mapped.length;
-
-    class method FromArray(aArray: array of Byte): Binary;
-  end;
-  {$ENDIF}
-
 
 implementation
 
+{ Range }
+
 class method Range.MakeRange(aLocation: Integer; aLength: Integer): Range;
-begin
+begin  
   exit new Range(Location := aLocation, Length := aLength);
 end;
 
-{$IF COOPER}
+{ Binary }
+
 method Binary.Assign(aData: Binary);
 begin
+  {$IF COOPER OR ECHOES}
   Clear;
   if aData <> nil then
-    fData.write(ArrayUtils.ToSignedArray(aData.ToArray), 0, aData.Length);
+    fData.write(aData.ToArray, 0, aData.Length);
+  {$ELSEIF NOUGAT}
+  mapped.setData(aData);
+  {$ENDIF}
 end;
 
-method Binary.ReadRangeOfBytes(Range: Range): array of Byte;
+method Binary.Read(Range: Range): array of Byte;
 begin
   if Range.Length = 0 then
     exit [];
 
-  if Range.Location + Range.Length > fData.size then
-    raise new SugarArgumentOutOfRangeException(String.Format(ErrorMessage.OUT_OF_RANGE_ERROR, Range.Location, Range.Length, fData.size));
+  if Range.Location + Range.Length > self.Length then
+    raise new SugarArgumentOutOfRangeException(String.Format(ErrorMessage.OUT_OF_RANGE_ERROR, Range.Location, Range.Length, self.Length));
 
   result := new Byte[Range.Length];
+  {$IF COOPER}  
   System.arraycopy(fData.toByteArray, Range.Location, result, 0, Range.Length);
-end;
-
-method Binary.ReadBytes(aLength: Integer): array of Byte;
-begin
-  result := ReadRangeOfBytes(Range.MakeRange(0, Math.Min(aLength, fData.size)));
-end;
-
-method Binary.Subdata(Range: Range): Binary;
-begin
-  var lData := ReadRangeOfBytes(Range);
-  exit Binary.FromArray(lData);
-end;
-
-method Binary.WriteBytes(aData: array of Byte; aLength: Integer);
-begin
-  fData.write(ArrayUtils.ToSignedArray(aData), 0, aLength);
-end;
-
-method Binary.WriteData(aData: Binary);
-begin
-  WriteBytes(aData.ToArray, aData.Length);
-end;
-
-method Binary.ToArray: array of Byte;
-begin
-  exit ArrayUtils.ToUnsignedArray(fData.toByteArray);
-end;
-
-method Binary.Clear;
-begin
-  fData.reset;
-end;
-
-class method Binary.FromArray(aArray: array of Byte): Binary;
-begin
-  result := new Binary;
-  result.WriteBytes(aArray, aArray.length);
-end;
-{$ELSEIF ECHOES}
-method Binary.Assign(aData: Binary);
-begin
-  Clear;
-  if aData <> nil then
-    fData.Write(aData.ToArray, 0, aData.Length);
-end;
-
-method Binary.ReadRangeOfBytes(Range: Range): array of Byte;
-begin
-  if Range.Length = 0 then
-    exit [];
-
-  if Range.Location + Range.Length > fData.Length then
-    raise new SugarArgumentOutOfRangeException(String.Format(ErrorMessage.OUT_OF_RANGE_ERROR, Range.Location, Range.Length, fData.Length));
-
-  result := new Byte[Range.Length];
+  {$ELSEIF ECHOES}
   var lPosition := fData.Position;
   fData.Position := Range.Location;
   try
@@ -171,104 +88,93 @@ begin
   finally
     fData.Position := lPosition;
   end;
+  {$ELSEIF NOUGAT}
+  mapped.getBytes(result) range(Range);
+  {$ENDIF}
 end;
 
-method Binary.ReadBytes(aLength: Integer): array of Byte;
+method Binary.Read(aLength: Integer): array of Byte;
 begin
-  result := ReadRangeOfBytes(Range.MakeRange(0, Math.Min(aLength, fData.Length)));
+  exit &Read(Range.MakeRange(0, Math.Min(aLength, self.Length)));
 end;
 
 method Binary.Subdata(Range: Range): Binary;
 begin
-  var lData := ReadRangeOfBytes(Range);
-  exit Binary.FromArray(lData);
+  exit Binary.FromArray(&Read(Range));
 end;
 
-method Binary.WriteBytes(aData: array of Byte; aLength: Integer);
-begin
-  fData.Write(aData, 0, aLength);
-end;
-
-method Binary.WriteData(aData: Binary);
-begin
-  WriteBytes(aData.ToArray, aData.Length);
-end;
-
-method Binary.ToArray: array of Byte;
-begin
-  exit fData.ToArray;
-end;
-
-method Binary.Clear;
-begin
-  fData.SetLength(0);
-end;
-
-class method Binary.FromArray(aArray: array of Byte): Binary;
-begin
-  result := new Binary;
-  result.WriteBytes(aArray, aArray.Length);
-end;
-
-{$ELSEIF NOUGAT}
-
-method Binary.ToArray: array of Byte;
-begin
-  result := new Byte[mapped.length];
-  mapped.getBytes(result) length(mapped.length);
-end;
-
-method Binary.Subdata(Range: Range): Binary;
-begin
-  exit Foundation.NSMutableData.dataWithData(mapped.subdataWithRange(Range));
-end;
-
-method Binary.ReadBytes(aLength: Integer): array of Byte;
-begin
-  result := ReadRangeOfBytes(Range.MakeRange(0, Math.Min(aLength, mapped.Length)));
-end;
-
-method Binary.WriteBytes(aData: array of Byte; aLength: Integer);
+method Binary.Write(aData: array of Byte; aLength: Integer);
 begin
   if aData = nil then
-    raise new SugarArgumentNullException("aData");
+    raise new SugarArgumentNullException("Data");
 
   if aLength = 0 then
     exit;
 
   if aLength > RemObjects.Oxygene.System.length(aData) then
-    raise new SugarArgumentOutOfRangeException(String.Format("Length {0} exceeds data length {1}", aLength, RemObjects.Oxygene.System.length(aData)));
+    raise new SugarArgumentOutOfRangeException("Length {0} exceeds data length {1}", aLength, RemObjects.Oxygene.System.length(aData));
 
+  {$IF COOPER}
+  fData.write(aData, 0, aLength);
+  {$ELSEIF ECHOES}
+  fData.Write(aData, 0, aLength);
+  {$ELSEIF NOUGAT}
   mapped.appendBytes(aData) length(aLength);
+  {$ENDIF}  
 end;
 
-method Binary.ReadRangeOfBytes(Range: Range): array of Byte;
+method Binary.Write(aData: Binary);
 begin
-  if Range.Length = 0 then
-    exit [];
-
-  if Range.Location + Range.Length > mapped.length then
-    raise new SugarArgumentOutOfRangeException(String.Format(ErrorMessage.OUT_OF_RANGE_ERROR, Range.Location, Range.Length, mapped.length));
-
-  result := new Byte[Range.Length];
-  mapped.getBytes(result) range(Range);
-end;
-
-method Binary.WriteData(aData: Binary);
-begin
-  if aData = nil then
-    raise new SugarArgumentNullException("aData");
-
+  SugarArgumentNullException.RaiseIfNil(aData, "Data");
+  {$IF COOPER OR ECHOES}
+  &Write(aData.ToArray, aData.Length);
+  {$ELSEIF NOUGAT}
   mapped.appendData(aData);
+  {$ENDIF}  
+end;
+
+method Binary.ToArray: array of Byte;
+begin
+  {$IF COOPER}
+  exit fData.toByteArray;
+  {$ELSEIF ECHOES}
+  exit fData.ToArray;
+  {$ELSEIF NOUGAT}
+  result := new Byte[mapped.length];
+  mapped.getBytes(result) length(mapped.length);
+  {$ENDIF}  
+end;
+
+method Binary.Clear;
+begin
+  {$IF COOPER}
+  fData.reset;
+  {$ELSEIF ECHOES}
+  fData.SetLength(0);
+  {$ELSEIF NOUGAT}
+  mapped.setLength(0);
+  {$ENDIF}  
 end;
 
 class method Binary.FromArray(aArray: array of Byte): Binary;
 begin
   if aArray = nil then
-    raise new SugarArgumentNullException("aArray");
+    raise new SugarArgumentNullException("Array");
 
+  {$IF COOPER}
+  result := new Binary;
+  result.Write(aArray, aArray.length);
+  {$ELSEIF ECHOES}
+  result := new Binary;
+  result.Write(aArray, aArray.Length);
+  {$ELSEIF NOUGAT}
   exit NSMutableData.dataWithBytes(aArray) length(length(aArray)); 
+  {$ENDIF}  
 end;
-{$ENDIF}
+
+class method Binary.&Empty: Binary;
+begin
+  exit FromArray([]);
+end;
 
 end.
