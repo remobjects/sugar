9ba287169a342aa0ff4a92c788aefb5f0524c10b
ejay <ejay@remobjects.com>
2013-11-29 18:18:41 +0200

    Cleanup for Stack class

--- Sugar/Collections/Stack.pas
+++ Sugar/Collections/Stack.pas
@@ -8,86 +8,40 @@ uses
   RemObjects.Oxygene.Sugar;
 
 type
-  {$IF ECHOES OR COOPER}
-  Stack<T> = public class mapped to {$IF ECHOES}System.Collections.Generic.Stack<T>{$ELSE}java.util.Stack<T>{$ENDIF}
+  Stack<T> = public class mapped to {$IF COOPER}java.util.Stack<T>{$ELSEIF ECHOES}System.Collections.Generic.Stack<T>{$ELSEIF NOUGAT}Foundation.NSMutableArray{$ENDIF}
   public
     constructor; mapped to constructor();
 
-    method Contains(Item: T): Boolean; mapped to Contains(Item);
-    method Clear; mapped to Clear;
-    method Peek: T; mapped to Peek;
-    method Pop: T; mapped to Pop;
-    method Push(Item: T);
-    method ToArray: array of T; {$IF ECHOES}mapped to ToArray;{$ENDIF}
-    method ForEach(Action: Action<T>);
-
-    property Count: Integer read {$IF ECHOES}mapped.Count{$ELSE}mapped.size{$ENDIF};
-  end;
-  {$ELSEIF NOUGAT}
-  Stack<T> = public class mapped to Foundation.NSMutableArray
-  public
-    constructor; mapped to constructor();
-
-    method Contains(Item: T): Boolean; mapped to containsObject(Item);
-    method Clear; mapped to removeAllObjects;
-    method Peek: T; 
+    method Contains(Item: T): Boolean;
+    method Clear;
+    method Peek: T;
     method Pop: T;
-    method Push(Item: T); mapped to addObject(Item);
+    method Push(Item: T);
     method ToArray: array of T;
     method ForEach(Action: Action<T>);
 
-    property Count: Integer read mapped.count;
+    property Count: Integer read {$IF COOPER}mapped.size{$ELSEIF ECHOES OR NOUGAT}mapped.Count{$ENDIF};
   end;
-  {$ENDIF}
 
 implementation
 
-{$IF COOPER OR ECHOES}
-method Stack<T>.Push(Item: T);
+method Stack<T>.Contains(Item: T): Boolean;
 begin
-  if Item = nil then
-    raise new RemObjects.Oxygene.Sugar.SugarArgumentNullException("Item");
-
-  mapped.Push(Item);
+  {$IF COOPER OR ECHOES}
+  exit mapped.Contains(Item);
+  {$ELSE}
+  exit mapped.containsObject(Item);
+  {$ENDIF}
 end;
-{$ENDIF}
 
-{$IF COOPER}
-method Stack<T>.ToArray: array of T;
-begin  
-  result := mapped.toArray(new T[0]);
-  //reverse
-  for i: Integer := 0 to result.length div 2 do begin
-    var temp := result[i];
-    result[i] := result[result.length - 1 - i];
-    result[result.length - 1 - i] := temp;
-  end;
-end;
-{$ELSEIF NOUGAT}
-method Stack<T>.Pop: T;
+method Stack<T>.Clear;
 begin
-  if mapped.count = 0 then
-    raise new SugarStackEmptyException("Stack is empty");
-
-  result := mapped.lastObject;
-  mapped.removeLastObject;
+  {$IF COOPER OR ECHOES}
+  mapped.Clear;
+  {$ELSE}
+  mapped.removeAllObjects;
+  {$ENDIF}
 end;
-
-method Stack<T>.Peek: T;
-begin
-  if mapped.count = 0 then
-    raise new SugarStackEmptyException("Stack is empty");
-
-  exit mapped.lastObject;
-end;
-
-method Stack<T>.ToArray: array of T;
-begin
-  result := new T[mapped.count];
-  for i: Integer := mapped.Count - 1 downto 0 do
-    result[mapped.count - i - 1] := mapped.objectAtIndex(i);
-end;
-{$ENDIF}
 
 method Stack<T>.ForEach(Action: Action<T>);
 begin
@@ -97,6 +51,62 @@ begin
   var Items := ToArray;
   for i: Integer := 0 to length(Items) - 1 do
     Action(Items[i]);
+end;
+
+method Stack<T>.Peek: T;
+begin
+  if self.Count = 0 then
+    raise new SugarStackEmptyException("Stack is empty");
+  
+  {$IF COOPER OR ECHOES}
+  exit mapped.Peek;
+  {$ELSE}
+  exit mapped.lastObject;
+  {$ENDIF}
+end;
+
+method Stack<T>.Pop: T;
+begin
+  if self.Count = 0 then
+    raise new SugarStackEmptyException("Stack is empty");
+
+  {$IF COOPER OR ECHOES}
+  exit mapped.Pop;
+  {$ELSE}
+  result := mapped.lastObject;
+  mapped.removeLastObject;
+  {$ENDIF}
+end;
+
+method Stack<T>.Push(Item: T);
+begin
+  if Item = nil then
+    raise new SugarArgumentNullException("Item");
+
+  {$IF COOPER OR ECHOES}
+  mapped.Push(Item);
+  {$ELSE}
+  mapped.addObject(Item);
+  {$ENDIF}
+end;
+
+method Stack<T>.ToArray: array of T;
+begin
+  {$IF COOPER}
+  result := mapped.toArray(new T[0]);
+
+  for i: Integer := 0 to result.length div 2 do begin
+    var temp := result[i];
+    result[i] := result[result.length - 1 - i];
+    result[result.length - 1 - i] := temp;
+  end;
+  {$ELSEIF ECHOES}
+  exit mapped.ToArray;
+  {$ELSEIF NOUGAT}
+  result := new T[mapped.count];
+  for i: Integer := mapped.Count - 1 downto 0 do
+    result[mapped.count - i - 1] := mapped.objectAtIndex(i);
+  {$ENDIF}
 end;
 
 end.
