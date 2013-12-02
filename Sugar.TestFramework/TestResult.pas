f064e94491ffa3a1223066cd9bc47f045320cfd0
ejay <ejay@remobjects.com>
2013-12-02 18:08:40 +0200

    ToString methods for test results

--- Sugar.TestFramework/TestResult.pas
+++ Sugar.TestFramework/TestResult.pas
@@ -8,6 +8,9 @@ type
     property Name: String read private write;
     property Failed: Boolean read private write;
     property Message: String read private write;
+
+    method {$IF NOUGAT}description{$ELSEIF COOPER}ToString{$ELSEIF ECHOES}ToString{$ENDIF}: String; override;
+
     constructor(aName: String; aFailed: Boolean; aMessage: String);
   end;
 
@@ -15,6 +18,9 @@ type
   public
     property Name: String read private write;
     property TestResults: List<TestResult> read private write;
+
+    method {$IF NOUGAT}description{$ELSEIF COOPER}ToString{$ELSEIF ECHOES}ToString{$ENDIF}: String; override;
+
     constructor (aName: String);
   end;
 
@@ -27,10 +33,23 @@ begin
   Message := aMessage;
 end;
 
+method TestResult.{$IF NOUGAT}description{$ELSEIF COOPER}ToString{$ELSEIF ECHOES}ToString{$ENDIF}: String;
+begin
+  if Failed then
+    exit Name+" Result: Failed Error: "+Message
+  else
+    exit Name+" Result: Succeed";
+end;
+
 constructor TestcaseResult(aName: String);
 begin
   Name := aName;
   TestResults := new List<TestResult>;
+end;
+
+method TestcaseResult.{$IF NOUGAT}description{$ELSEIF COOPER}ToString{$ELSEIF ECHOES}ToString{$ENDIF}: String;
+begin
+  exit Name+" Count: "+TestResults.Count.{$IF NOUGAT}description{$ELSE}ToString{$ENDIF};
 end;
 
 end.
