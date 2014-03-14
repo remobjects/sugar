7fd186aacc71a4983db0bd83e7038640e0a42d6e
ejay <ejay@remobjects.com>
2014-03-14 13:55:32 +0200

    Encoding class (Cooper)

--- Sugar/Encoding.pas
+++ Sugar/Encoding.pas
@@ -3,7 +3,7 @@
 interface
 
 type
-  Encoding = public class {$IF COOPER}{$ELSEIF ECHOES}mapped to System.Text.Encoding{$ELSEIF NOUGAT}{$ENDIF}
+  Encoding = public class {$IF COOPER}mapped to java.nio.charset.Charset{$ELSEIF ECHOES}mapped to System.Text.Encoding{$ELSEIF NOUGAT}{$ENDIF}
   public
     method GetBytes(Value: array of Char): array of Byte;
     method GetBytes(Value: array of Char; Offset: Integer; Count: Integer): array of Byte;
@@ -28,6 +28,7 @@ implementation
 method Encoding.GetBytes(Value: array of Char): array of Byte;
 begin
   {$IF COOPER}
+  exit GetBytes(Value, 0, Value.length);
   {$ELSEIF ECHOES}
   exit mapped.GetBytes(Value);
   {$ELSEIF NOUGAT}
@@ -37,6 +38,9 @@ end;
 method Encoding.GetBytes(Value: array of Char; Offset: Integer; Count: Integer): array of Byte;
 begin
   {$IF COOPER}
+  var Buffer := mapped.encode(java.nio.CharBuffer.wrap(Value, Offset, Count));
+  result := new Byte[Buffer.remaining];
+  Buffer.get(result);
   {$ELSEIF ECHOES}
   exit mapped.GetBytes(Value, Offset, Count);
   {$ELSEIF NOUGAT}
@@ -46,6 +50,9 @@ end;
 method Encoding.GetBytes(Value: String): array of Byte;
 begin
   {$IF COOPER}
+  var Buffer := mapped.encode(Value);
+  result := new Byte[Buffer.remaining];
+  Buffer.get(result);
   {$ELSEIF ECHOES}
   exit mapped.GetBytes(Value);
   {$ELSEIF NOUGAT}
@@ -55,6 +62,13 @@ end;
 method Encoding.GetChars(Value: array of Byte; Offset: Integer; Count: Integer): array of Char;
 begin
   {$IF COOPER}
+  var Buffer := mapped.newDecoder.
+    onMalformedInput(java.nio.charset.CodingErrorAction.REPLACE).
+    onUnmappableCharacter(java.nio.charset.CodingErrorAction.REPLACE).
+    replaceWith("?").
+    decode(java.nio.ByteBuffer.wrap(Value, Offset, Count));
+  result := new Char[Buffer.remaining];
+  Buffer.get(result);
   {$ELSEIF ECHOES}
   exit mapped.GetChars(Value, Offset, Count);
   {$ELSEIF NOUGAT}
@@ -64,6 +78,7 @@ end;
 method Encoding.GetChars(Value: array of Byte): array of Char;
 begin
   {$IF COOPER}
+  exit GetChars(Value, 0, Value.length);
   {$ELSEIF ECHOES}
   exit mapped.GetChars(Value);
   {$ELSEIF NOUGAT}
@@ -73,6 +88,7 @@ end;
 method Encoding.GetString(Value: array of Byte): String;
 begin
   {$IF COOPER}
+  exit GetString(Value, 0, Value.length);
   {$ELSEIF ECHOES}
   exit mapped.GetString(Value);
   {$ELSEIF NOUGAT}
@@ -82,6 +98,12 @@ end;
 method Encoding.GetString(Value: array of Byte; Offset: Integer; Count: Integer): String;
 begin
   {$IF COOPER}
+  var Buffer := mapped.newDecoder.
+    onMalformedInput(java.nio.charset.CodingErrorAction.REPLACE).
+    onUnmappableCharacter(java.nio.charset.CodingErrorAction.REPLACE).
+    replaceWith("?").
+    decode(java.nio.ByteBuffer.wrap(Value, Offset, Count));
+  exit Buffer.toString;
   {$ELSEIF ECHOES}
   exit mapped.GetString(Value, Offset, Count);
   {$ELSEIF NOUGAT}
@@ -91,6 +113,7 @@ end;
 class method Encoding.GetEncoding(Name: String): Encoding;
 begin
   {$IF COOPER}
+  exit java.nio.charset.Charset.forName(Name);
   {$ELSEIF ECHOES}
   exit System.Text.Encoding.GetEncoding(Name);
   {$ELSEIF NOUGAT}
