a3706f0895d1860732836110338c36ca14adafdc
ejay <ejay@remobjects.com>
2014-03-17 18:00:13 +0200

    Enable Encoding unit test for Nougat

--- Sugar.Tests/Main/Echoes/Program.pas
+++ Sugar.Tests/Main/Echoes/Program.pas
@@ -16,7 +16,7 @@ implementation
 
 class method ConsoleApp.Main(args: array of System.String);
 begin  
-  var results := TestRunner.RunAll;
+  var results := TestRunner.RunAll();
   var output := new StringPrinter(results);
   System.Diagnostics.Debug.WriteLine(output.Result);
 end;
--- Sugar.Tests/Main/OSX/Program.pas
+++ Sugar.Tests/Main/OSX/Program.pas
@@ -9,14 +9,14 @@ uses
 type
   Program = public static class
   public
-    method Main(aArguments: array of String): Int32;
+    method Main(aArguments: array of String): Int32;   
   end;
 
 implementation
 
 method Program.Main(aArguments: array of String): Int32;
 begin
-  var results := TestRunner.RunAll([new DictionaryTest]);
+  var results := TestRunner.RunAll();
   var output := new StringPrinter(results);  
   NSLog("%@", output.Result);
   //giving time for output to catch our log
