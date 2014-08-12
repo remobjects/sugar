de1693eb6cbd8de9fa59dd0a6a0d44d0c6ebea51
ejay <ejay@remobjects.com>
2014-08-12 17:51:44 +0300

    Updated unit test for crypto Utils class

--- Sugar.Tests/Tests/Cryptography/Utils.pas
+++ Sugar.Tests/Tests/Cryptography/Utils.pas
@@ -5,12 +5,10 @@ interface
 uses
   Sugar,
   Sugar.Cryptography,
-  Sugar.TestFramework;
+  RemObjects.Elements.EUnit;
 
 type
-  UtilsTest = public class (Testcase)
-  private
-    method AreEquals(Expected, Actual: array of Byte);
+  UtilsTest = public class (Test)
   public
     method ToHexString;
     method FromHexString;
@@ -18,45 +16,38 @@ type
 
 implementation
 
-method UtilsTest.AreEquals(Expected: array of Byte; Actual: array of Byte);
-begin
-  Assert.CheckInt(Expected.Length, Actual.Length);
-  for i: Integer := 0 to Expected.Length - 1 do
-    Assert.CheckInt(Expected[i], Actual[i]);
-end;
-
 method UtilsTest.ToHexString;
 begin
   var Data: array of Byte := [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-  Assert.CheckString("000102030405060708090A0B0C0D0E0F", Utils.ToHexString(Data));
-  Assert.CheckString("0001020304", Utils.ToHexString(Data, 5));
-  Assert.CheckString("090A0B", Utils.ToHexString(Data, 9, 3));
-  Assert.CheckString("", Utils.ToHexString(Data, 0));
+  Assert.AreEqual(Utils.ToHexString(Data), "000102030405060708090A0B0C0D0E0F");
+  Assert.AreEqual(Utils.ToHexString(Data, 5), "0001020304");
+  Assert.AreEqual(Utils.ToHexString(Data, 9, 3), "090A0B");
+  Assert.AreEqual(Utils.ToHexString(Data, 0), "");
 
-  Assert.CheckString("D41D8CD98F00B204E9800998ECF8427E", Utils.ToHexString(new MessageDigest(DigestAlgorithm.MD5).Digest([])));
-  Assert.CheckString("F7FF9E8B7BB2E09B70935A5D785E0CC5D9D0ABF0", Utils.ToHexString(new MessageDigest(DigestAlgorithm.SHA1).Digest(Encoding.UTF8.GetBytes("Hello"))));
+  Assert.AreEqual(Utils.ToHexString(MessageDigest.ComputeHash([], DigestAlgorithm.MD5)), "D41D8CD98F00B204E9800998ECF8427E");
+  Assert.AreEqual(Utils.ToHexString(MessageDigest.ComputeHash(Encoding.UTF8.GetBytes("Hello"), DigestAlgorithm.SHA1)), "F7FF9E8B7BB2E09B70935A5D785E0CC5D9D0ABF0");
 
-  Assert.IsException(->Utils.ToHexString(nil));
-  Assert.IsException(->Utils.ToHexString(Data, -1));
-  Assert.IsException(->Utils.ToHexString(Data, 55));
-  Assert.IsException(->Utils.ToHexString(Data, -1, 1));
-  Assert.IsException(->Utils.ToHexString(Data, 1, -1));
-  Assert.IsException(->Utils.ToHexString(Data, 54, 1));
-  Assert.IsException(->Utils.ToHexString(Data, 1, 54));
+  Assert.Throws(->Utils.ToHexString(nil));
+  Assert.Throws(->Utils.ToHexString(Data, -1));
+  Assert.Throws(->Utils.ToHexString(Data, 55));
+  Assert.Throws(->Utils.ToHexString(Data, -1, 1));
+  Assert.Throws(->Utils.ToHexString(Data, 1, -1));
+  Assert.Throws(->Utils.ToHexString(Data, 54, 1));
+  Assert.Throws(->Utils.ToHexString(Data, 1, 54));
 end;
 
 method UtilsTest.FromHexString;
 begin
   var Data: String := "000102030405060708090A0B0C0D0E0F";
-  AreEquals([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], Utils.FromHexString(Data));
-  AreEquals(new MessageDigest(DigestAlgorithm.MD5).Digest([]), Utils.FromHexString("D41D8CD98F00B204E9800998ECF8427E"));
-  AreEquals([], Utils.FromHexString(""));
+  Assert.AreEqual(Utils.FromHexString(Data), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+  Assert.AreEqual(Utils.FromHexString("D41D8CD98F00B204E9800998ECF8427E"), MessageDigest.ComputeHash([], DigestAlgorithm.MD5));
+  Assert.AreEqual(Utils.FromHexString(""), []);
 
-  Assert.IsException(->Utils.FromHexString(nil));
-  Assert.IsException(->Utils.FromHexString("FFF"));
-  Assert.IsException(->Utils.FromHexString("FFFZ"));
-  Assert.IsException(->Utils.FromHexString(#13#10));
+  Assert.Throws(->Utils.FromHexString(nil));
+  Assert.Throws(->Utils.FromHexString("FFF"));
+  Assert.Throws(->Utils.FromHexString("FFFZ"));
+  Assert.Throws(->Utils.FromHexString(#13#10));
 end;
 
 end.
