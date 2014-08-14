355e7e7eec88932586ac2d1058c69801770f7840
ejay <ejay@remobjects.com>
2014-08-14 17:49:23 +0300

    Updated unit test for Folder class

--- Sugar.Tests/Tests/IO/Folder.pas
+++ Sugar.Tests/Tests/IO/Folder.pas
@@ -5,10 +5,10 @@ interface
 uses
   Sugar,
   Sugar.IO,
-  Sugar.TestFramework;
+  RemObjects.Elements.EUnit;
 
 type
-  FolderTest = public class (Testcase)
+  FolderTest = public class (Test)
   private
     Data: Folder;
   public
@@ -33,7 +33,7 @@ implementation
 method FolderTest.Setup;
 begin
   Data := Folder.UserLocal.CreateFolder("SugarTest", true);
-  Assert.IsNotNull(Data);
+  Assert.IsNotNil(Data);
 end;
 
 method FolderTest.TearDown;
@@ -43,162 +43,162 @@ end;
 
 method FolderTest.CreateFile;
 begin
-  Assert.IsNull(Data.GetFile("Sample.txt"));
+  Assert.IsNil(Data.GetFile("Sample.txt"));
   var Value := Data.CreateFile("Sample.txt", true);
-  Assert.IsNotNull(Value);
-  Assert.IsNotNull(Data.GetFile("Sample.txt"));
+  Assert.IsNotNil(Value);
+  Assert.IsNotNil(Data.GetFile("Sample.txt"));
 
   Value := Data.CreateFile("Sample.txt", false);
-  Assert.IsNotNull(Value);
-  Assert.CheckInt(1, length(Data.GetFiles));
+  Assert.IsNotNil(Value);
+  Assert.AreEqual(length(Data.GetFiles), 1);
 
-  Assert.IsException(->Data.CreateFile("Sample.txt", true));
-  Assert.IsException(->Data.CreateFile("/", true));
-  Assert.IsException(->Data.CreateFile(nil, true));
+  Assert.Throws(->Data.CreateFile("Sample.txt", true));
+  Assert.Throws(->Data.CreateFile("/", true));
+  Assert.Throws(->Data.CreateFile(nil, true));
 end;
 
 method FolderTest.CreateFolder;
 begin
-  Assert.IsNull(Data.GetFolder("Sample"));
+  Assert.IsNil(Data.GetFolder("Sample"));
   var Value := Data.CreateFolder("Sample", true);
-  Assert.IsNotNull(Value);
-  Assert.IsNotNull(Data.GetFolder("Sample"));
+  Assert.IsNotNil(Value);
+  Assert.IsNotNil(Data.GetFolder("Sample"));
 
   Value := Data.CreateFolder("Sample", false);
-  Assert.IsNotNull(Value);
-  Assert.CheckInt(1, length(Data.GetFolders));
+  Assert.IsNotNil(Value);
+  Assert.AreEqual(length(Data.GetFolders), 1);
 
-  Assert.IsException(->Data.CreateFolder("Sample", true));
-  Assert.IsException(->Data.CreateFolder("/", true));
-  Assert.IsException(->Data.CreateFolder(nil, true));
+  Assert.Throws(->Data.CreateFolder("Sample", true));
+  Assert.Throws(->Data.CreateFolder("/", true));
+  Assert.Throws(->Data.CreateFolder(nil, true));
 end;
 
 method FolderTest.Delete;
 begin  
-  Assert.IsNull(Data.GetFolder("Sample"));
+  Assert.IsNil(Data.GetFolder("Sample"));
   var Value := Data.CreateFolder("Sample", true);
-  Assert.IsNotNull(Value);
-  Assert.IsNotNull(Data.GetFolder("Sample"));
-  Assert.CheckInt(1, length(Data.GetFolders));
+  Assert.IsNotNil(Value);
+  Assert.IsNotNil(Data.GetFolder("Sample"));
+  Assert.AreEqual(length(Data.GetFolders), 1);
   Value.Delete;
-  Assert.IsNull(Data.GetFolder("Sample"));
-  Assert.CheckInt(0, length(Data.GetFolders));
-  Assert.IsException(->Value.Delete);
+  Assert.IsNil(Data.GetFolder("Sample"));
+  Assert.AreEqual(length(Data.GetFolders), 0);
+  Assert.Throws(->Value.Delete);
 end;
 
 method FolderTest.GetFile;
 begin
-  Assert.IsNull(Data.GetFile("Sample.txt"));
-  Assert.IsNotNull(Data.CreateFile("Sample.txt", true));
+  Assert.IsNil(Data.GetFile("Sample.txt"));
+  Assert.IsNotNil(Data.CreateFile("Sample.txt", true));
   var Value := Data.GetFile("Sample.txt");
-  Assert.IsNotNull(Value);
-  Assert.IsException(->Data.GetFile(nil));
+  Assert.IsNotNil(Value);
+  Assert.Throws(->Data.GetFile(nil));
 end;
 
 method FolderTest.GetFiles;
 begin
-  Assert.CheckInt(0, length(Data.GetFolders));
-  Assert.CheckInt(0, length(Data.GetFiles));
+  Assert.AreEqual(length(Data.GetFolders), 0);
+  Assert.AreEqual(length(Data.GetFiles), 0);
   //setup
   Data.CreateFile("1", true);
   Data.CreateFile("2", true);
   Data.CreateFile("3", true);
   Data.CreateFolder("Temp1", true);
   Data.CreateFolder("Temp2", true);
-  Assert.CheckInt(2, length(Data.GetFolders));
-  Assert.CheckInt(3, length(Data.GetFiles));
+  Assert.AreEqual(length(Data.GetFolders), 2);
+  Assert.AreEqual(length(Data.GetFiles), 3);
 
   var Expected := new Sugar.Collections.List<String>;
   Expected.Add("1");
   Expected.Add("2");
   Expected.Add("3");
-  var Value := Data.GetFiles;
-  Assert.CheckInt(3, length(Value));
+  var Value := Data.GetFiles;  
+  Assert.AreEqual(length(Value), 3);
   for i: Integer := 0 to length(Value) - 1 do
-    Assert.CheckBool(true, Expected.Contains(Value[i].Name));
+    Assert.IsTrue(Expected.Contains(Value[i].Name));
 end;
 
 method FolderTest.GetFolder;
 begin
-  Assert.IsNull(Data.GetFolder("Sample"));
-  Assert.IsNotNull(Data.CreateFolder("Sample", true));
+  Assert.IsNil(Data.GetFolder("Sample"));
+  Assert.IsNotNil(Data.CreateFolder("Sample", true));
   var Value := Data.GetFolder("Sample");
-  Assert.IsNotNull(Value);
-  Assert.IsException(->Data.GetFolder(nil));
+  Assert.IsNotNil(Value);
+  Assert.Throws(->Data.GetFolder(nil));
 end;
 
 method FolderTest.GetFolders;
 begin
-  Assert.CheckInt(0, length(Data.GetFolders));
-  Assert.CheckInt(0, length(Data.GetFiles));
+  Assert.AreEqual(length(Data.GetFolders), 0);
+  Assert.AreEqual(length(Data.GetFiles), 0);
   //setup
   Data.CreateFile("1", true);
   Data.CreateFile("2", true);
   Data.CreateFile("3", true);
   Data.CreateFolder("Temp1", true);
   Data.CreateFolder("Temp2", true);
-  Assert.CheckInt(2, length(Data.GetFolders));
-  Assert.CheckInt(3, length(Data.GetFiles));
+  Assert.AreEqual(length(Data.GetFolders), 2);
+  Assert.AreEqual(length(Data.GetFiles), 3);
 
   var Expected := new Sugar.Collections.List<String>;
   Expected.Add("Temp1");
   Expected.Add("Temp2");
   var Value := Data.GetFolders;
-  Assert.CheckInt(2, length(Value));
+  Assert.AreEqual(length(Value), 2);
   for i: Integer := 0 to length(Value) - 1 do
-    Assert.CheckBool(true, Expected.Contains(Value[i].Name));
+    Assert.IsTrue(Expected.Contains(Value[i].Name));
 end;
 
 method FolderTest.Rename;
 begin
   var Value := Data.CreateFolder("Sample", true);
-  Assert.IsNotNull(Value);
-  Assert.IsNotNull(Value.CreateFolder("Test", true));
-  Assert.IsNotNull(Value.CreateFile("1.txt", true));
+  Assert.IsNotNil(Value);
+  Assert.IsNotNil(Value.CreateFolder("Test", true));
+  Assert.IsNotNil(Value.CreateFile("1.txt", true));
   var OldPath := Value.Path;
   Value.Rename("Temp");
-  Assert.CheckBool(true, OldPath <> Value.Path);
-  Assert.IsNull(Data.GetFolder("Sample"));
-  Assert.IsNotNull(Data.GetFolder("Temp"));
+  Assert.IsTrue(OldPath <> Value.Path);
+  Assert.IsNil(Data.GetFolder("Sample"));
+  Assert.IsNotNil(Data.GetFolder("Temp"));
 
-  Assert.IsException(->Value.Rename("Temp"));
+  Assert.Throws(->Value.Rename("Temp"));
   Data.CreateFolder("Temp1", true);
-  Assert.IsException(->Value.Rename("Temp1"));
-  Assert.IsException(->Value.Rename("/"));
-  Assert.IsException(->Value.Rename(nil));
-  Assert.IsException(->Value.Rename(""));
+  Assert.Throws(->Value.Rename("Temp1"));
+  Assert.Throws(->Value.Rename("/"));
+  Assert.Throws(->Value.Rename(nil));
+  Assert.Throws(->Value.Rename(""));
 end;
 
 method FolderTest.FromPath;
 begin  
-  Assert.IsNotNull(Data.CreateFolder("Sample", true));
-  Assert.IsNotNull(new Folder(Path.Combine(Data.Path, "Sample")));
-  Assert.IsException(->new Folder(Path.Combine(Data.Path, "Unknown")));
-  Assert.IsException(->new Folder(nil));
+  Assert.IsNotNil(Data.CreateFolder("Sample", true));
+  Assert.IsNotNil(new Folder(Path.Combine(Data.Path, "Sample")));
+  Assert.Throws(->new Folder(Path.Combine(Data.Path, "Unknown")));
+  Assert.Throws(->new Folder(nil));
 end;
 
 method FolderTest.UserLocal;
 begin
-  Assert.IsNotNull(Folder.UserLocal);
+  Assert.IsNotNil(Folder.UserLocal);
 end;
 
 method FolderTest.PathProperty;
 begin
-  Assert.CheckBool(false, String.IsNullOrEmpty(Data.Path));
-  Assert.IsNotNull(Data.CreateFolder("Sample", true));
+  Assert.IsFalse(String.IsNullOrEmpty(Data.Path));
+  Assert.IsNotNil(Data.CreateFolder("Sample", true));
   var Value := new Folder(Path.Combine(Data.Path, "Sample"));
-  Assert.IsNotNull(Value);
-  Assert.CheckBool(false, String.IsNullOrEmpty(Value.Path));
+  Assert.IsNotNil(Value);
+  Assert.IsFalse(String.IsNullOrEmpty(Value.Path));
 end;
 
 method FolderTest.Name;
 begin
-  Assert.CheckBool(false, String.IsNullOrEmpty(Data.Name));
-  Assert.IsNotNull(Data.CreateFolder("Sample", true));
+  Assert.IsFalse(String.IsNullOrEmpty(Data.Name));
+  Assert.IsNotNil(Data.CreateFolder("Sample", true));
   var Value := new Folder(Path.Combine(Data.Path, "Sample"));
-  Assert.IsNotNull(Value);
-  Assert.CheckBool(false, String.IsNullOrEmpty(Value.Name));
-  Assert.CheckString("Sample", Value.Name);
+  Assert.IsNotNil(Value);
+  Assert.IsFalse(String.IsNullOrEmpty(Value.Name));
+  Assert.AreEqual(Value.Name, "Sample");
 end;
 
 end.
