74e51f8bc65e1b01ce4ecbbc36497ecfcd89fb3f
marc hoffman <mh@remobjects.com>
2015-08-03 10:38:15 +0200

    Utils.ToHexString (untested)

--- Sugar/Cryptography/Utils.pas
+++ Sugar/Cryptography/Utils.pas
@@ -14,6 +14,8 @@ type
     method ToHexString(Data: array of Byte; Count: Integer): String;
     method ToHexString(Data: array of Byte): String;
     method FromHexString(Data: String): array of Byte;
+
+    method ToHexString(Value: Int32; Width: Integer := 0): String;
   end;
 
 implementation
@@ -77,6 +79,33 @@ begin
 
   if (result > 15) or (result < 0) then
     raise new SugarFormatException("{0}. Invalid character: [{1}]", ErrorMessage.FORMAT_ERROR, C);
+end;
+
+method Utils.ToHexString(Value: Int32; Width: Integer): String;
+begin
+  if Width mod 2 ≠ 0 then Width := Width+1;
+
+  if Width > 16 then Width := 16
+  else if Value > $ffff ffff ffff ff and Width < 16 then Width := 16
+  else if Value > $ffff ffff ffff and Width < 14 then Width := 14
+  else if Value > $ffff ffff ff and Width < 12 then Width := 12
+  else if Value > $ffff ffff and Width < 10 then Width := 10
+  else if Value > $ffff ff and Width < 8 then Width := 8
+  else if Value > $ffff and Width < 6 then Width := 6
+  else if Value > $ff and Width < 2 then Width := 4
+  else Width := 2;
+  
+  result := '';
+  for i: Integer := Width/2 - 1 downto 0 do begin
+
+    var lCurrentByte := Value shr i mod $ff;
+
+    var Num := lCurrentByte shr 4  and $f;
+    result := result + chr(55 + Num + (((Num - 10) shr 31) and -7));
+    Num := lCurrentByte and $f;
+    result := result + chr(55 + Num + (((Num - 10) shr 31) and -7));
+    
+  end;
 end;
 
 end.
