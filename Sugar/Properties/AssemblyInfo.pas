c965b66137c8a3bc6eb5486b07f160e709dcfd1c
marc hoffman <mh@remobjects.com>
2016-04-21 10:14:06 -0400

    AssemblyInfo file was not being used

--- Sugar/Properties/AssemblyInfo.pas
+++ Sugar/Properties/AssemblyInfo.pas
@@ -5,17 +5,17 @@ interface
 uses
   System.Reflection,
   System.Resources,
+  System.Runtime.CompilerServices,  
   System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle('RemObjects Sugar')]
-[assembly: AssemblyDescription('RemObjects Sugar Corss-PLatform Library')]
+[assembly: AssemblyDescription('RemObjects Sugar Cross-Platform Library for Elements')]
 [assembly: AssemblyCompany('RemObjects Software')]
 [assembly: AssemblyProduct('')]
 [assembly: AssemblyCopyright('RemObjects Software')]
 [assembly: AssemblyTrademark('')]
 [assembly: AssemblyCulture('')]
 [assembly: AssemblyVersion('1.0.0.1')]
-[assembly: NeutralResourcesLanguage('')]
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyDelaySign(false)]
