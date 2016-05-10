a98ee873accd251fff377ed4f89e30efab503734
marc hoffman <mh@remobjects.com>
2016-05-10 12:33:18 -0400

    Dispatch.Async()

---
+++ Sugar/Dispatch.pas
@@ -0,0 +1,20 @@
+﻿namespace Sugar;
+
+interface
+
+type
+  Dispatch = public static class
+  private
+  protected
+  public
+    method &Async(aBlock: block);
+  end;
+
+implementation
+
+method Dispatch.Async(aBlock: block);
+begin
+  async aBlock();
+end;
+
+end.
