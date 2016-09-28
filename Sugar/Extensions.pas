5ed86b0ec61a10875cc32f0bf6b7a7c6f7c5eac0
marc hoffman <mh@remobjects.com>
2016-09-28 13:15:12 -0400

    `ToString` shaped consistently return a native string, not `Sugar.String`, on all platforms,.

--- Sugar/Extensions.pas
+++ Sugar/Extensions.pas
@@ -3,11 +3,11 @@
 interface
 
 {$IF COOPER}
-extension method java.lang.Object.«ToString»: String;
+extension method java.lang.Object.«ToString»: java.lang.String;
 extension method java.lang.Object.«Equals»(aOther: Object): Boolean;
 extension method java.lang.Object.GetHashCode: Integer;
 {$ELSEIF TOFFEE}
-extension method Foundation.NSObject.ToString: String;
+extension method Foundation.NSObject.ToString: Foundation.NSString;
 extension method Foundation.NSObject.Equals(Obj: Object): Boolean;
 extension method Foundation.NSObject.GetHashCode: Integer;
 {$ENDIF}
@@ -15,7 +15,7 @@ extension method Foundation.NSObject.GetHashCode: Integer;
 implementation
 
 {$IF COOPER}
-extension method java.lang.Object.«ToString»: String;
+extension method java.lang.Object.«ToString»: java.lang.String;
 begin
   result := «toString»();
 end;
@@ -30,7 +30,7 @@ begin
   result := hashCode;
 end;
 {$ELSEIF TOFFEE}
-extension method Foundation.NSObject.ToString: String;
+extension method Foundation.NSObject.ToString: Foundation.NSString;
 begin
   result := description;
 end;
